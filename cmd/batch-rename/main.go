4fdf61170e536d36fe46ef6843165f6e72d90bda
BlinkSpark <wangning2813@foxmal.com>
2019-07-19 16:40:16 +0800

    batch rename

---
+++ cmd/batch-rename/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	util "github.com/blinkspark/go-blink-util"
+	"log"
+	"os"
+)
+
+func main() {
+	in := flag.String("in", "", "-in target%2d.mp4 (target file name you want to rename)")
+	out := flag.String("out", "", "-out to%2d.mp4 (target file name you want to rename)")
+	min := flag.Int("min", 0, "-min 1 (min value of template)")
+	max := flag.Int("max", 0, "-max 10 (min value of template)")
+
+	util.Ignore(in, out, min, max)
+
+	for n := *min; n <= *max; n++ {
+		inFname := fmt.Sprintf(*in, n)
+		outFname := fmt.Sprintf(*out, n)
+		err := os.Rename(inFname, outFname)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
